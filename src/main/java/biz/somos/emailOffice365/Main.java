4420ba240583c88380924cff751555c4a3b370f1
Brayam Otero <davidotero@unicauca.edu.co>
2022-12-26 19:16:17 -0500

    feat: moderm authentication with msal4j

--- src/main/java/biz/somos/emailOffice365/Main.java
+++ src/main/java/biz/somos/emailOffice365/Main.java
@@ -5,8 +5,28 @@
  */
 package biz.somos.emailOffice365;
 
+import com.microsoft.aad.msal4j.ClientCredentialFactory;
+import com.microsoft.aad.msal4j.ClientCredentialParameters;
+import com.microsoft.aad.msal4j.ConfidentialClientApplication;
+import com.microsoft.aad.msal4j.PublicClientApplication;
+import com.microsoft.aad.msal4j.UserNamePasswordParameters;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
+import java.util.StringJoiner;
 import javax.mail.Authenticator;
 import javax.mail.Message;
 import javax.mail.MessagingException;
@@ -40,17 +60,26 @@ public class Main {
     private static boolean isSSL = false;
     private static boolean isStartTLS = false;
     
-    private static String mechanismsAuth = "PLAIN";
+    private static String mechanismsAuth = "";
     
     private static boolean isAuth = false;
     
     private static String emailTo;
     
     private static String trustHost;
+    private static boolean getToken = false;
+    private static String tenantID;
+    private static String clientID;
+    private static String grantType;
+    private static String scope;
+    private static String clientSecret;
+    private static boolean library = false;
+    private static boolean ROPC = false;
+    private static boolean clientCred = false;
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         // TODO code application logic here
         if (args == null){
             System.out.println("Especifique el protocolo");
@@ -106,8 +135,37 @@ public class Main {
                 case "-trusthost":
                     trustHost = values[1];
                     break;
+                case "-tenant":
+                    tenantID = values[1];
+                    break;
+                case "-clientID":
+                    clientID = values[1];
+                    break;
+                case "-grantType":
+                    grantType = values[1];
+                    break;
+                case "-scope":
+                    scope = values[1];
+                    break;
+                case "-lib":
+                    library = true;
+                    break;
+                case "-token":
+                    getToken = true;
+                    break;
+                case "-ROPC":
+                    ROPC = true;                    
+                    break;
+                case "-clientCred":
+                    clientCred = true;
+                    break;
             }                                                
                 
+        }
+        
+        if (getToken){
+            getToken();
+            return;
         }
         if(testIMAP){
             if (isImaps)
@@ -128,7 +186,7 @@ public class Main {
             System.out.println("------------TEST SMTP--------------");
             System.out.println("PROTOCOL: " + protocol);
             sendSTMP();
-        }
+        }        
     }
     
     public static void readIMAP(){
@@ -143,7 +201,7 @@ public class Main {
         try {
             /* Create the session and get the store for read the mail. */
 
-            Session session = Session.getInstance(System.getProperties());
+            Session session = Session.getInstance(props);
             session.setDebug(true);
             
             Store store = session.getStore(protocol);
@@ -173,7 +231,13 @@ public class Main {
         if(isAuth)
             props.setProperty("mail."+protocol+".auth", "true"); 
         if (trustHost != null)
-            props.setProperty("mail."+protocol+".ssl.trust", trustHost); 
+            props.setProperty("mail."+protocol+".ssl.trust", trustHost);
+        if (!mechanismsAuth.isEmpty()){
+            props.setProperty("mail."+protocol+".auth.mechanisms", mechanismsAuth);
+            props.put("mail."+protocol+".auth.login.disable", "true");
+            props.put("mail."+protocol+".auth.plain.disable", "true");
+            //props.put("mail."+protocol+".ssl.enable", "true");
+        }            
         
         props.put("mail.debug", "true");
         props.put("mail.debug.auth", "true");
@@ -215,4 +279,162 @@ public class Main {
         }
     }
     
+    private static void getToken() throws Exception{       
+        if (library && ROPC){
+            System.out.println("----- Libreria msal4j con flujo ROPC -----");
+            try{
+                System.out.print("Ingrese authority: ");                
+                String authority = System.console().readLine();
+                System.out.print("Ingrese clientID: ");                
+                clientID = System.console().readLine();                
+                //String authority = "https://login.microsoftonline.com/" + tenantID;
+                System.out.print("Ingrese scope: ");                
+                scope = System.console().readLine();                
+                System.out.print("Ingrese email: ");                
+                email_user = System.console().readLine();
+                System.out.println("Email user: " + email_user);                
+                PublicClientApplication app = PublicClientApplication.builder(clientID).authority(authority).build();                
+                System.out.print("Ingrese contraseña: ");            
+                char[] passChar = System.console().readPassword();
+                UserNamePasswordParameters parameters = UserNamePasswordParameters.builder(Collections.singleton(scope), email_user, passChar).build();
+                String token = app.acquireToken(parameters).get().accessToken();
+                System.out.println("Token obtenido: " + token);
+                return;
+            }catch(Exception ex){
+                System.out.println("Hay una excepción de tipo - " + ex.getClass());
+                System.out.println("Exception message - " + ex.getMessage());
+                throw ex;
+            }
+        } 
+        if (library && clientCred){
+            System.out.println("----- Libreria msal4j con flujo de concesión de credenciales de cliente -----");
+            try{
+                System.out.print("Ingrese authority: ");                
+                String authority = System.console().readLine();
+                System.out.print("Ingrese clientID: ");                
+                clientID = System.console().readLine();                
+                //String authority = "https://login.microsoftonline.com/" + tenantID;
+                System.out.print("Ingrese scope: ");                
+                scope = System.console().readLine();
+                System.out.print("Ingrese client secret: ");                
+                clientSecret = System.console().readLine();
+                
+                ConfidentialClientApplication app = ConfidentialClientApplication.builder(
+                        clientID,
+                        ClientCredentialFactory.createFromSecret(clientSecret))
+                        .authority(authority)                        
+                        .build();
+                ClientCredentialParameters clientCredentialParam = ClientCredentialParameters.builder(
+                        Collections.singleton(scope))
+                        .build();
+                String token = app.acquireToken(clientCredentialParam).get().accessToken();
+                System.out.println("Token obtenido: " + token);
+            }catch (Exception ex){
+                System.out.println("Hay una excepción de tipo - " + ex.getClass());
+                System.out.println("Exception message - " + ex.getMessage());
+                throw ex;
+            }
+        }
+        if(!library && ROPC){
+            System.out.println("----- Sin uso de la librería con flujo ROPC -----");
+            try{
+                System.out.print("Ingrese authority: ");                
+                String authority = System.console().readLine();
+                System.out.print("Ingrese clientID: ");                
+                clientID = System.console().readLine();                
+                //String authority = "https://login.microsoftonline.com/" + tenantID;
+                System.out.print("Ingrese scope: ");                
+                scope = System.console().readLine();                
+                System.out.print("Ingrese email: ");                
+                email_user = System.console().readLine();
+                System.out.print("Ingrese client secret: ");                
+                clientSecret = Arrays.toString(System.console().readPassword());
+                
+                //URL url = new URL("https://login.microsoftonline.com/" + tenantID + "/oauth2/v2.0/token");
+                System.out.print("Please input the password for: ");            
+                char[] passChar = System.console().readPassword();
+                password = Arrays.toString(passChar);
+
+                Map<String,String> arguments = new HashMap<>();
+                arguments.put("client_id", clientID);
+                //arguments.put("scope", "user.read openid profile offline_access");
+                arguments.put("scope", scope);
+                if(!clientSecret.isEmpty() || clientSecret != null)
+                    arguments.put("client_secret", clientSecret);
+                arguments.put("username", email_user);
+                arguments.put("password", password);
+                arguments.put("grant_type", "password");
+                
+                String url_str = authority + "/oauth2/v2.0/token";
+                sendRequest(url_str, arguments);
+            }catch(Exception e){
+                System.out.println("Error: " + e);
+            }      
+        }
+        if (!library && clientCred){
+            System.out.println("----- Sin uso de la librería con flujo de concesión de credenciales de cliente -----");
+            try{                
+                System.out.print("Ingrese authority: ");                
+                String authority = System.console().readLine();
+                System.out.print("Ingrese clientID: ");                
+                clientID = System.console().readLine();                
+                //String authority = "https://login.microsoftonline.com/" + tenantID;
+                System.out.print("Ingrese scope: ");                
+                scope = System.console().readLine();
+                System.out.print("Ingrese client secret: ");                
+                clientSecret = System.console().readLine();
+                Map<String,String> arguments = new HashMap<>();
+                arguments.put("client_id", clientID);
+                arguments.put("scope", scope);
+                arguments.put("client_secret", clientSecret);
+                arguments.put("grant_type", "client_credentials");
+                
+                String url_str = authority + "/oauth2/v2.0/token";
+                sendRequest(url_str, arguments);
+            }catch(Exception e){
+                System.out.println("Error: " + e);
+            }
+            
+        }
+    }
+    
+    private static void sendRequest(String url_str, Map<String,String> arguments) throws MalformedURLException, IOException{
+        URL url = new URL(url_str);
+        URLConnection con = url.openConnection();
+        HttpURLConnection http = (HttpURLConnection)con;
+        http.setRequestMethod("POST"); // PUT is another valid option
+        http.setDoOutput(true);        
+
+        StringJoiner sj = new StringJoiner("&");
+        for(Map.Entry<String,String> entry : arguments.entrySet())
+            sj.add(URLEncoder.encode(entry.getKey(), "UTF-8") + "=" 
+                 + URLEncoder.encode(entry.getValue(), "UTF-8"));
+        byte[] out = sj.toString().getBytes(StandardCharsets.UTF_8);
+        int length = out.length;
+        http.setFixedLengthStreamingMode(length);
+        http.setRequestProperty("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+        http.connect();
+        try(OutputStream os = http.getOutputStream()) {
+            os.write(out);
+        }
+        try(BufferedReader br = new BufferedReader(
+            new InputStreamReader(http.getInputStream(), "utf-8"))) {
+            StringBuilder response = new StringBuilder();
+            String responseLine = null;
+            while ((responseLine = br.readLine()) != null) {
+                response.append(responseLine.trim());
+            }
+            System.out.println(response.toString());
+        }catch(Exception e){
+            try(BufferedReader br = new BufferedReader(
+            new InputStreamReader(http.getErrorStream(), "utf-8"))) {
+                StringBuilder response = new StringBuilder();
+                String responseLine = null;
+                while ((responseLine = br.readLine()) != null) {
+                    response.append(responseLine.trim());
+                }
+            System.out.println("Error: " + response.toString());
+            }
+        }
+    }
 }
